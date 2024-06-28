db15035cf753aef03a44d5fe96d06a17f05746f9
Skorp321 <Skorp321@yandex.ru>
2024-06-28 17:54:27 +0300

    create yolo homography

--- Deep-EIoU/Deep-EIoU/tools/utils/homographer.py
+++ Deep-EIoU/Deep-EIoU/tools/utils/homographer.py
@@ -2,6 +2,7 @@ import json
 import os
 import cv2
 import numpy as np
+import pandas as pd
 from ultralytics import YOLO
 import ffmpegcv
 from loguru import logger
@@ -10,68 +11,63 @@ from loguru import logger
 class HomographySetup:
     def __init__(self, config):
         self.config = config
-        self.layout_img, self.first_frame = self.load_and_prepare_images()
+        self.layout_img, self.first_frame, self.points_names = self.load_and_prepare_images()
         self.points_layout = []
         self.points_frame = []
+        self.data = pd.DataFrame()
 
     def load_and_prepare_images(self):
         layout_img = cv2.imread(self.config.path_to_field)
         cap = ffmpegcv.VideoCapture(self.config.path_to_field)
-        # cap = cv2.VideoCapture(self.config.path)
+        with open(self.config.path_to_field_points) as file:
+            keypoints_field = json.load(file)
 
+        names = keypoints_field["categories"][0]["keypoints"]
+        
         if not cap.isOpened():
             print("Error: Could not open file.")
             return None, None
 
         ret, frame = cap.read()
         if not ret:
-            print("Error: Could not read the video frame.123")
+            print("Error: Could not read the video frame.")
             return None, None
 
         cap.release()
 
-        return layout_img, frame
-
-    def get_points_from_layout(self, soccer_field_path_anno: str) -> list:
-        sorted_dict = self._extracted_from_compute_homography_matrix_3(
-            soccer_field_path_anno
-        )
-        return list(sorted_dict.values())
+        return layout_img, frame, names
 
     def compute_homography_matrix(self):
-        if self.config.h_matrix_path and os.path.exists(self.config.h_matrix_path):
-            print("Try to finde homography matrixe")
-            print(np.load(self.config.h_matrix_path))
-        else:
-            model_keypoint = YOLO(self.config.path_to_keypoints_det)
-            outputs = model_keypoint(self.first_frame, half=True, device=0, imgsz=1280)[0]
-            field_points = outputs.keypoints[0].xy.detach().cpu().tolist()
-            '''print(outputs.keypoints)
-            sorted_dict = self._extracted_from_compute_homography_matrix_3(
-                "/container_dir/panorama/data/Swiss_vs_Slovakia-panoramic_video_anno/annotations/person_keypoints_default.json"
-            )
-            field_points = list(sorted_dict.values())'''
-            layout_points = self.get_points_from_layout(
-                self.config.path_to_field_points
-            )
+        model_keypoint = YOLO(self.config.path_to_keypoints_det)
+        outputs = model_keypoint(self.first_frame, half=True, device=0, imgsz=1280)[0]
+        keypoints = outputs.keypoints.data[0].detach().cpu().numpy()
+        
+        layout_points = self._extracted_from_compute_homography_matrix_3(
+            self.config.path_to_field_points
+        )
+        
+        df = pd.DataFrame(keypoints, index=self.names, columns=["x", "y", "conf"])
+        df_n = df[df["conf"] >= 0.75]
+        field_points = layout_points[layout_points.index.isin(df_n.index)]
 
-            H, _ = cv2.findHomography(np.array(field_points), np.array(layout_points))
-            print(H)
-            self.config.h_matrix_path = "/container_dir/panorama/data/h_matrix_path.npy"
-            np.save(self.config.h_matrix_path, H)
+        H, _ = cv2.findHomography(df_n.values, field_points.values)
 
-        return np.load(self.config.h_matrix_path)
+        '''self.config.h_matrix_path = "/container_dir/panorama/data/h_matrix_path.npy"
+        np.save(self.config.h_matrix_path, H)'''
+
+        return H
 
     # TODO Rename this here and in `get_points_from_layout` and `compute_homography_matrix`
-    def _extracted_from_compute_homography_matrix_3(self, arg0):
-        with open(arg0, "r") as file:
-            data = file.read()
-        data_dict = json.loads(data)
-        categoris = data_dict["categories"][0]["keypoints"]
-        df = data_dict["annotations"][0]["keypoints"]
-        res = {data: df[i * 3 : (i + 1) * 3 - 1] for i, data in enumerate(categoris)}
-        sorted_list = sorted(res.items(), key=lambda x: x[0])
-        return dict(sorted_list)
+    def _extracted_from_compute_homography_matrix_3(self, path_to_field):
+        with open(path_to_field) as file:
+            keypoints_map_pos = json.load(file)
+        field_points = np.array(keypoints_map_pos["annotations"][0]["keypoints"]).reshape(-1, 3)
+
+        field_df = pd.DataFrame(
+            field_points,
+            index=self.names,
+            columns=["x", "y", "viz"])
+        return field_df
 
     def prepare_images_for_display(self, frame, layout):
         logger.info(f"layout:{layout.shape}, frame: {frame.shape}")
--- src/data/test2.py
+++ src/data/test2.py
@@ -1,16 +1,72 @@
+import pandas as pd
+import numpy as np
+import json
 from ultralytics import YOLO
 import cv2
 
-model = YOLO("models/yolov8m_keypoints.pt")
-img = cv2.imread("data/Panoramic_video_football.mp4")
-res = model.predict(img, imgsz=1280)
+file_path = "C:\\Users\\PotapovS\\Documents\\Projects\\panorama\\data\\cvat\\Panoramic_video_football\\annotations\\person_keypoints_default.json"
+img_path = "C:\\Users\\PotapovS\\Documents\\Projects\\panorama\\data\\img2.jpg"
+field_path = "C:\\Users\\PotapovS\\Documents\\Projects\\panorama\\data\\field\\annotations\\person_keypoints_default.json"
+field_img_pth = 'C:\\Users\\PotapovS\\Documents\\Projects\\panorama\\data\\field.png'
+detektor_path = 'C:\\Users\\PotapovS\\Documents\\Projects\\panorama\\models\\yolov8m_goalkeeper.pt'
 
-points = res[0].keypoints.xy[0].cpu().numpy()
-#image_with_keypoints = cv2.drawKeypoints(img, points, 0, (0, 0, 255), flags=cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
-for h_point in points:
-    x, y = int(h_point[0]), int(h_point[1])
-    cv2.circle(img, (x, y), 6, (0, 0, 255), -1)
+data = pd.DataFrame()
 
-cv2.imwrite('image.jpg', img)
+model = YOLO(
+    "C:\\Users\\PotapovS\\Documents\\Projects\\panorama\\models\\yolov8m_keypoints.pt"
+)
+det_model = YOLO(detektor_path)
+
+det = det_model(img_path, imgsz=1280)
+res = model(img_path, imgsz=1280)
+
+boxes = det[0].boxes.xywh.cpu().numpy()
+keypoints = res[0].keypoints.data[0].cpu().numpy()
+
+boxes = pd.DataFrame(boxes)
+data['x'] = boxes[0] 
+data['y']  = boxes[1] + boxes[3] / 2
+data["x"] = data["x"].astype(int)
+data["y"] = data["y"].astype(int)
+
+with open(file_path) as f:
+    keypoints_map_pos = json.load(f)
+
+with open(field_path) as file:
+    keypoints_field = json.load(file)
+
+names = keypoints_map_pos["categories"][0]["keypoints"]
+field_points = np.array(keypoints_field["annotations"][0]["keypoints"]).reshape(-1, 3)
+
+field_df = pd.DataFrame(
+    field_points,
+    index=names,
+    columns=["x", "y", "viz"],
+)
+
+df = pd.DataFrame(keypoints, index=names, columns=["x", "y", "conf"])
+df_n = df[df["conf"] >= 0.75]
+field_df_filtered = field_df[field_df.index.isin(df_n.index)]
+
+img = cv2.imread(img_path)
+img_field = cv2.imread(field_img_pth)
+
+homog, mask = cv2.findHomography(df_n.values, field_df_filtered.values)   
+
+pred_dst_pts = []                                                  # Initialize players tactical map coordiantes list
+for pt in data.values:                                             # Loop over players frame coordiantes
+    pt = np.append(np.array(pt), np.array([1]), axis=0)            # Covert to homogeneous coordiantes
+    dest_point = np.matmul(homog, pt)                              # Apply homography transofrmation
+    dest_point = 2*dest_point/dest_point[2]                        # Revert to 2D-coordiantes
+    pred_dst_pts.append(dest_point[:2])                            # Update players tactical map coordiantes list
+pred_dst_pts = np.array(pred_dst_pts)
+    
+for item in boxes.values:
+    cv2.rectangle(img, (int(item[0]-item[2]/2), int(item[1]-item[3]/2)), (int(item[0]+item[2]/2), int(item[1]+item[3]/2)), (255,255,255), 1)
+    
+for item in pred_dst_pts:
+    cv2.circle(img_field, (int(item[0]), int(item[1])), 6, (0, 0, 255), -(1))
+
+cv2.imshow('field', img_field)
+cv2.imshow("image", img)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
